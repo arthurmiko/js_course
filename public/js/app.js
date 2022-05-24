8e6affd3ae7e6176a21653a30be985dde6372f46
Arthur Miko <arthurmiko@gmail.com>
2022-05-24 12:07:59 +0300

    move app.js to public

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
 console.log('script from public app.js');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetch('http://localhost:3000/weather_forecast?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.forecast)
+
+        const pLocation = document.querySelector('#location');
+        const pForecast = document.querySelector('#forecast');
+        pLocation.textContent = 'Location: ' + data.location;
+        pForecast.textContent = 'Forecast: ' + data.forecast;
+      }
+    })
+  });
+});
